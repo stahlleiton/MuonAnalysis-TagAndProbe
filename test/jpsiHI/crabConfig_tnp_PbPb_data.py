f04038a50ca72ba6580c870858603454cdc5ecac
Andre Govinda Stahl Leiton <andre.govinda.stahl.leiton@cern.ch>
2016-08-13 04:53:04 +0200

    updated the crab config files

--- test/jpsiHI/crabConfig_tnp_PbPb_data.py
+++ test/jpsiHI/crabConfig_tnp_PbPb_data.py
@@ -2,24 +2,24 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 config.section_('General')
-config.General.requestName = 'TNP_SingleMuLowPt_Run2015E_PromptReco_v1_AOD_20160811'
+config.General.requestName = 'TNP_HIOniaTnP_HIRun2015_PromptReco_v1_AOD_20160813'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.section_('JobType')
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'tnp_pp_data.py'
+config.JobType.psetName = 'tnp_PbPb_data.py'
 config.JobType.maxMemoryMB = 2500
-config.JobType.outputFiles = ['tnpJPsi_Data_pp5TeV_AOD.root']
+config.JobType.outputFiles = ['tnpJPsi_Data_PbPb_AOD.root']
 
 config.section_('Data')
-config.Data.inputDataset = '/SingleMuLowPt/Run2015E-PromptReco-v1/AOD'
+config.Data.inputDataset = '/HIOniaTnP/HIRun2015-PromptReco-v1/AOD'
 config.Data.inputDBS = 'global'
 config.Data.unitsPerJob = 10
 config.Data.splitting = 'LumiBased'
-config.Data.runRange = '262081-262328'
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/5TeV/Cert_262081-262328_5TeV_PromptReco_Collisions15_25ns_JSON_MuonPhys.txt'
+config.Data.runRange = '262548-263757'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/Cert_262548-263757_PromptReco_HICollisions15_JSON_MuonPhys_v2.txt'
 config.Data.outLFNDirBase = '/store/group/phys_heavyions/%s/TagAndProbe2015/%s' % (getUsernameFromSiteDB(), config.General.requestName)
 config.Data.publication = False
 
