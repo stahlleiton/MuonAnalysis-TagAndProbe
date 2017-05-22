61cb6eca31098de6c1686b125178129c51b76681
okukral <ota.kukral@cern.ch>
2017-05-22 15:42:14 +0200

    Added macros to create html pages

---
+++ macros/CreateMultiplePages.sh
@@ -0,0 +1,8 @@
+./createHTMLpageAllPlots_pdf.sh  Trk_MC/Trk_1binSeg/
+./createHTMLpageAllPlots_pdf.sh  Trk_MC/Trk_centSeg/
+./createHTMLpageAllPlots_pdf.sh  Trk_MC/Trk_etaSeg/
+./createHTMLpageAllPlots_pdf.sh  Trk_MC/Trk_ptSeg/
+./createHTMLpageAllPlots_pdf.sh  Trk_RD/Trk_1binSeg/
+./createHTMLpageAllPlots_pdf.sh  Trk_RD/Trk_centSeg//
+./createHTMLpageAllPlots_pdf.sh  Trk_RD/Trk_etaSeg/
+./createHTMLpageAllPlots_pdf.sh  Trk_RD/Trk_ptSeg/
