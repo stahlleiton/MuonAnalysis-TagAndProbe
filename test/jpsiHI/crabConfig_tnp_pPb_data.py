740f9567ba6685ac197cbaef40b5e1c087c9a091
Jared Jay <jsjay@ucdavis.edu>
2018-08-21 21:24:58 +0200

    work in progress

--- test/jpsiHI/crabConfig_tnp_pPb_data.py
+++ test/jpsiHI/crabConfig_tnp_pPb_data.py
@@ -2,25 +2,29 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 config.section_('General')
-config.General.requestName = 'TNP_HIOniaTnP_HIRun2015_PromptReco_v1_AOD_20160813'
+config.General.requestName = 'TNP_HIOniaTnP_HIRun2016_PromptReco_v1_AOD_20180813'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = False
 
 config.section_('JobType')
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'tnp_PbPb_data.py'
+config.JobType.psetName = 'tnp_pPb_data.py'
 config.JobType.maxMemoryMB = 2500
-config.JobType.outputFiles = ['tnpJPsi_Data_PbPb_AOD.root']
+config.JobType.outputFiles = ['tnpJPsi_Data_Pbp_AOD.root']
 
 config.section_('Data')
-config.Data.inputDataset = '/HIOniaTnP/HIRun2015-PromptReco-v1/AOD'
+config.Data.inputDataset = '/PASingleMuon/PARun2016C-PromptReco-v1/AOD'
 config.Data.inputDBS = 'global'
 config.Data.unitsPerJob = 5
 config.Data.splitting = 'LumiBased'
-config.Data.runRange = '262548-263757'
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/Cert_262548-263757_PromptReco_HICollisions15_JSON_MuonPhys_v2.txt'
-config.Data.outLFNDirBase = '/store/group/phys_heavyions/%s/TagAndProbe2015/%s' % (getUsernameFromSiteDB(), config.General.requestName)
+#Pbp (1st run)
+config.Data.runRange = '285479-285832'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/HI/Cert_285479-285832_HI8TeV_PromptReco_pPb_Collisions16_JSON_NoL1T.txt'
+#pPb (2nd run)
+#config.Data.runRange = '285952-286496'
+#config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/HI/Cert_285952-286496_HI8TeV_PromptReco_Pbp_Collisions16_JSON_NoL1T.txt'
+config.Data.outLFNDirBase = '/store/group/phys_heavyions/dileptons/%s/TagAndProbe2016/%s' % (getUsernameFromSiteDB(), config.General.requestName)
 config.Data.publication = False
 
 config.section_('Site')
