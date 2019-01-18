fd7e4ad5fd8289107fa6f88413fb0b45e724e7ab
Jared Jay <jsjay@ucdavis.edu>
2019-01-18 01:05:32 +0100

    added code to submit condor jobs

---
+++ test/jpsiHI/condor/condorExec.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /afs/cern.ch/work/j/jjay/public/TagAndProbe_pPb/CMSSW_8_0_30/src
+eval `scramv1 runtime -sh`
+cd MuonAnalysis/TagAndProbe/test/jpsiHI
+cmsRun tp_Ana_MuId_$1_pPb.py $2 
