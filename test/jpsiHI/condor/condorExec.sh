68d131471b9f34a0610f27a080944ec1b1a38b59
Jared Jay <jsjay@ucdavis.edu>
2019-02-01 23:26:32 +0100

    updated condor code

--- test/jpsiHI/condor/condorExec.sh
+++ test/jpsiHI/condor/condorExec.sh
@@ -2,4 +2,4 @@
 cd /afs/cern.ch/work/j/jjay/public/TagAndProbe_pPb/CMSSW_8_0_30/src
 eval `scramv1 runtime -sh`
 cd MuonAnalysis/TagAndProbe/test/jpsiHI
-cmsRun tp_Ana_MuId_$1_pPb.py $2 
+cmsRun tp_Ana_$2_$1_pPb.py $3 
